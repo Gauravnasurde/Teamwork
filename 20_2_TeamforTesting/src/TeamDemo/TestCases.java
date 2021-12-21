e57a5d28701c466ddd568e20b4c23750eca2da05
Shree <Shree@DESKTOP-DSJJ1OM>
2021-12-21 12:01:14 +0530

    Team commit

---
+++ 20_2_TeamforTesting/src/TeamDemo/TestCases.java
@@ -0,0 +1,12 @@
+package TeamDemo;
+
+public class TestCases 
+{
+	public void M1()
+	{
+		System.out.println("Please write the test Cases For Mercury Login One by one");
+		System.out.println("Start from Userid");
+		
+	}
+
+}
